3e18149bf9f5c849e8e33bc802a621946920fb5d
margauxclrc6 <margx.cloarec@gmail.com>
2025-01-10 16:46:17 +0100

    ecriture de la classe company

--- src/main/java/base/Company.java
+++ src/main/java/base/Company.java
@@ -1,5 +1,72 @@
 package base;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
 
+    private final String name;
+    private final List<Employee> employees;
+    private final List<Project> projects;
+
+
+    public Company(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+        this.projects = new ArrayList<>();
+    }
+
+
+    public void addEmployee(Employee employee) {
+        if (!employees.contains(employee)) {
+            employees.add(employee);
+        }
+    }
+
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee);
+    }
+
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+
+    public void addProject(Project project) {
+        if (!projects.contains(project)) {
+            projects.add(project);
+        }
+    }
+
+
+    public void removeProject(Project project) {
+        projects.remove(project);
+    }
+
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", employees=" + employees.size() +
+                ", projects=" + projects.size() +
+                '}';
+    }
 }
+
