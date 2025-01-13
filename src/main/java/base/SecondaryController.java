b2c48416336f138d3372f39531b138c05059e913
log18 <yelena.grandjean@gmail.com>
2025-01-13 17:21:07 +0100

    secondary controller
    ça ne marche complètement (erreur mac)

--- src/main/java/base/SecondaryController.java
+++ src/main/java/base/SecondaryController.java
@@ -1,8 +1,64 @@
 package base;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 
 public class SecondaryController {
-    
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Hyperlink createAccount;
+    @FXML
+    private Hyperlink passwordReset;
+
+
+    @FXML
+    private void handleLoginButton(ActionEvent event) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        System.out.println("Username: "+ username + " Password: "+ password);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handleCreateAccount(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("third.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void handlePasswordReset(ActionEvent event) {
+        //Do Nothing for now
+    }
 }
